1d0475063e0d73a6cbe4a9ad86a5084a35a64104
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-02 07:38:15 -0500

    main method for simple testing

--- Traindoc.java
+++ Traindoc.java
@@ -7,6 +7,14 @@ public class Traindoc{
   private String route;
   private int routeNum,year,month,day;
 
+  public static void main(String[] args){
+    try{
+      Traindoc t = new Traindoc(args[0]);
+    }catch(FileNotFoundException e){
+      System.out.println(args[0]+" not found");
+    }
+  }
+
   public Traindoc(String filename)throws FileNotFoundException{
     File f = new File(filename);
     Scanner sca = new Scanner(f);

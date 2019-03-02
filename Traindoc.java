fef1abc8c5b678194a956b5f66e6b2bb31b059d5
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-02 07:32:18 -0500

    file access, scanner printing each buffer

--- Traindoc.java
+++ Traindoc.java
@@ -8,6 +8,10 @@ public class Traindoc{
   private int routeNum,year,month,day;
 
   public Traindoc(String filename)throws FileNotFoundException{
-
+    File f = new File(filename);
+    Scanner sca = new Scanner(f);
+    while(sca.hasNext()){
+      System.out.println(sca.next());
+    }
   }
 }

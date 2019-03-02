c2c86dd4a360adcc54dd54500216af072ad4ae23
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-02 07:26:43 -0500

    class structure and fields at file level for each daily train

---
+++ Traindoc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Traindoc{
+  private String filename;
+  private StationStop[] data;
+  private String route;
+  private int routeNum,year,month,day;
+
+  public Traindoc(String filename)throws FileNotFoundException{
+
+  }
+}
